dd99685b3d1d87ff9db777799f08356bfa69d33d
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-03-01 17:15:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-let imgCarr= document.querySelector("#firt")
+let imgCarr = document.querySelector("#firt")
 let avanco = 0
+let menu = document.querySelector("#menu")
+let close = document.querySelector(".close")
+let imgMain= document.querySelector(".imgMain")
+let number= 1
 
-function next(){
-  if(avanco != -1000){
+function next() {
+  if (avanco != -1000) {
     avanco -= 500
+    number += 1
   }
-  imgCarr.style.marginTop= avanco +"px"
+  imgCarr.style.marginTop = avanco + "px"
+
+  imgMain.setAttribute("src", "images/desktop-image-hero-"+ number +".jpg")
 }
 
-function prev(){
-  if(avanco != 0){
+function prev() {
+  if (avanco != 0) {
     avanco += 500
+    number -= 1
   }
-  imgCarr.style.marginTop= avanco +"px"
+  imgCarr.style.marginTop = avanco + "px"
+
+  imgMain.setAttribute("src", "images/desktop-image-hero-"+ number +".jpg")
+}
+
+function hambur() {
+  menu.style.zIndex = "1"
+}
+
+function fecha(){
+  menu.style.zIndex = "-1"
 }
